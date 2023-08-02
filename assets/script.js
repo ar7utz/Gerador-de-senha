8d014457ec1517a866891cadd7a5f899408206c5
ar7utz <pereira.artur@hotmail.com>
2023-08-01 22:50:21 -0300

    script concertado e funcionando 100%

--- assets/script.js
+++ assets/script.js
@@ -8,14 +8,14 @@ const checkUpper = document.querySelector("#checkUpper");
 const checkNum = document.querySelector("#checkNum");
 const checkSymb = document.querySelector("#checkSymb");
 
-const numbers = [0,1,2,3,4,5,6,7,8,9]; //array de números
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; //array de números
 const symbols = ["!", "@", "#", "$", "%", "&"]; //array dos símbolos
 
 const chars = Array.from(Array(24)).map((_, i) => i + 97);
 const lowercaseChar = chars.map((item) => String.fromCharCode(item));
 const uppercaseChar = lowercaseChar.map((item) => item.toUpperCase());
 
-numComprimento.innerHTML = lenPass.value;
+numComprimento.innerHTML = lenPass.value; //mostrar os números no range
 
 lenPass.addEventListener("change", () => {
     numComprimento.innerHTML = lenPass.value;
@@ -27,7 +27,7 @@ btnGerar.addEventListener("click", () => {
         checkSymb.checked,
         checkLower.checked,
         checkUpper.checked,
-        lenPass.checked.value
+        lenPass.value
     );
 });
 
@@ -36,22 +36,23 @@ const generatePassword = (
     hasSymb,
     hasLower,
     hasUpper,
-    comprimento
-) => {
-    const novaArray = {
-        ...(hasNum ? numbers : []),
-        ...(hasSymb ? symbols : []),
-        ...(hasLower ? lowercaseChar : []),
-        ...(hasUpper ? uppercaseChar : []),
+    length
+    ) => {
+        const novaArray = [
+            ...(hasNum ? numbers : []),
+            ...(hasSymb ? symbols : []),
+            ...(hasLower ? lowercaseChar : []),
+            ...(hasUpper ? uppercaseChar : []),
+        ];
+    
+        if (novaArray.length === 0) return; //verificar se tiver informação no array
+    
+        let senha = "";
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * novaArray.length); //math.floor para arredondar
+            senha += novaArray[randomIndex];
+    
+            inputPass.value = senha;
+    }
+    
     };
-};
-
-if (novaArray.comprimento === 0) return; //verificar se tiver informação no array
-
-let senha = "";
-for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * novaArray.comprimento); //math.floor para arredondar
-    senha += novaArray[randomIndex];
-
-    inputPass.value = senha;
-}
